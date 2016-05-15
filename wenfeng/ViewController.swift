5386e95727b1319de3263ec5329ce4fa22e94505
xuboxie <342898906@qq.com>
2016-05-15 16:30:04 +0800

    Initial Commit

---
+++ wenfeng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wenfeng
+//
+//  Created by BoxU on 16/5/15.
+//  Copyright © 2016年 BoxU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
